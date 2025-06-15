6777644af041d4f39381d3af2e06ee55c2e690c0
Satvir <sssatvir12@gmail.com>
2025-06-15 17:24:18 +0530

    4.7 Printing Complex Data Types

---
+++ complex_con.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+void main(){
+  //create json
+  Map<String,dynamic> name={
+    jsonEncode('first_name'):jsonEncode('satvir'),
+    jsonEncode('last_name'):jsonEncode('singh'),
+    jsonEncode('dob'):jsonEncode(1989)
+};
+
+ print(name);
+ //decode json
+  Map<String, dynamic> items = json.decode(name.toString());
+  print('$items');
+}
