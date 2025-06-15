c5688475ba407a110b055c7bd0113691b1f9eacd
Satvir <sssatvir12@gmail.com>
2025-06-15 17:42:47 +0530

    2.3 Iterating over a Range of Items

---
+++ chapter2_3.dart
@@ -0,0 +1,8 @@
+void main(){
+  int max_iterations=10;
+  for(var i=0;i<max_iterations;i++){
+    print(i);
+  }
+  List daysOfWeek = ['Sunday', 'Monday', 'Tuesday'];
+  daysOfWeek.forEach(print);
+}
