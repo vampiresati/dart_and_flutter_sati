071b8a6f46145032c386792e11dd9c74eec128c4
Satvir <sssatvir12@gmail.com>
2025-06-15 14:53:10 +0530

    1.7 Adding a Constant Variable

--- main.dart
+++ main.dart
@@ -1,13 +1,5 @@
 void main(){
-  String name="satvir";
-  print(name);
-  String city_address="""
-  #3489,
-  house no 188,
-  sirhind mandi,
-  """;
-  //utf-16
-  print('address $city_address');
-  int number=5;
+
+  const number=5;
   print('this is my number $number');
 }
