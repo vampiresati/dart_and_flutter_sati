7f23bc1f4fc6f625216aadf0fcce8c5aef382ff6
Satvir <sssatvir12@gmail.com>
2025-07-05 16:17:18 +0530

    switch statement

--- main.dart
+++ main.dart
@@ -1,5 +1,29 @@
 void main(){
 
-  List daysofweeks=["Sunday","Monday","Tuesday","Wednesday"];
+  int day =1;
+  String ?value;
+  switch(day){
+    case 0:
+      value="Sunday";
+      break;
+    case 1:
+      value="Monday";
+      break;
+    case 2:
+      value="Tuesday";
+      break;
+    case 3:
+      value="Wednesday";
+      break;
+    case 4:
+      value="Thursday";
+      break;
+    case 5:
+      value="Friday";
+      break;
+    Case 6:
+      value="Saturday";
 
+  }
+  print('day is $value')
 }
