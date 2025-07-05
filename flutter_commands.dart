72330e68f5efe9e04e3f784025783381e1e1f39e
Satvir <sssatvir12@gmail.com>
2025-07-05 16:36:59 +0530

    flutter commands

---
+++ flutter_commands.dart
@@ -0,0 +1,13 @@
+To create the default application type:
+flutter create my_awesome_app
+To create a module:
+flutter create -t module my_awesome_module
+To create a package:
+flutter create -t package my_awesome_package
+To create a plug-in:
+flutter create -t plugin my_awesome_plugin --platforms web --platform android
+flutter create -t skeleton my_awesome_skeleton
+flutter create -s widgets.GestureDetector.1 my_awesome_sample
+flutter run
+flutter pub get
+
